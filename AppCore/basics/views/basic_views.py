3a6fdb7544f6fa7e05354b9ee23cb4c4500d70eb
Lucas Soares <soareslukas9090@hotmail.com>
2025-10-13 21:41:10 -0300

    feat: implementar tratamento de exceções e mensagem de erro no BasicPostAPIView e BasicGetAPIView

--- AppCore/basics/views/basic_views.py
+++ AppCore/basics/views/basic_views.py
@@ -25,7 +25,7 @@ class BasicPostAPIView(GenericAPIView):
     success_message = ''
     
     def do_action_post(self, serializer, request):
-        pass
+        raise SystemErrorException("Este método não foi implementado.")
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
@@ -56,46 +56,73 @@ class BasicPostAPIView(GenericAPIView):
                 data, status=result.get('status_code', status.HTTP_200_OK)
             )
         except BusinessRuleException as err:
-            if str(err):
-                return Response(
-                    {'status': 'error', 'detail': str(err)}, status=status.HTTP_400_BAD_REQUEST
-                )
             return Response(
-                {'status': 'error', 'detail': RESPONSE_TENTE_NOVAMENTE}, status=status.HTTP_400_BAD_REQUEST
+                {'status': 'error', 'detail': str(err) or RESPONSE_TENTE_NOVAMENTE}, status=status.HTTP_400_BAD_REQUEST
             )
         except ValidationException as err:
-            if str(err):
-                return Response(
-                    {'status': 'error', 'detail': str(err)}, status=status.HTTP_400_BAD_REQUEST
-                )
             return Response(
-                {'status': 'error', 'detail': RESPONSE_ALGO_QUE_MANDOU_ESTA_ERRADO}, status=status.HTTP_400_BAD_REQUEST
+                {'status': 'error', 'detail': str(err) or RESPONSE_ALGO_QUE_MANDOU_ESTA_ERRADO}, status=status.HTTP_400_BAD_REQUEST
             )
         except AuthorizationException as err:
-            if str(err):
-                return Response(
-                    {'status': 'error', 'detail': str(err)}, status=status.HTTP_403_FORBIDDEN
-                )
             return Response(
-                {'status': 'error', 'detail': RESPONSE_VOCE_NAO_PODE_FAZER_ISSO}, status=status.HTTP_403_FORBIDDEN
-            )
-        except SystemErrorException as err:
-            if str(err):
-                return Response(
-                    {'status': 'error', 'detail': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
-            return Response(
-                {'status': 'error', 'detail': RESPONSE_VOCE_NAO_PODE_FAZER_ISSO}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                {'status': 'error', 'detail': str(err) or RESPONSE_VOCE_NAO_PODE_FAZER_ISSO}, status=status.HTTP_403_FORBIDDEN
             )
         except NotFoundException as err:
-            if str(err):
-                return Response(
-                    {'status': 'error', 'detail': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
             return Response(
-                {'status': 'error', 'detail': RESPONSE_ALGUM_DADO_NAO_FOI_ENCONTRADO}, status=status.HTTP_404_NOT_FOUND
+                {'status': 'error', 'detail': str(err) or RESPONSE_ALGUM_DADO_NAO_FOI_ENCONTRADO}, status=status.HTTP_404_NOT_FOUND
             )
-        
+        except SystemErrorException as err:
+            return Response(
+                {'status': 'error', 'detail': str(err) or RESPONSE_VOCE_NAO_PODE_FAZER_ISSO}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        except Exception as err:
+            return Response(
+                {'status': 'error', 'detail': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+            
+class BasicGetAPIView(GenericAPIView):
+    http_method_names = ['get']
+    success_message = ''
+    
+    def validate_get(self, request, *args, **kwargs):
+        pass
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.validate_get(request, *args, **kwargs)
+
+            data = {'status': 'success'}
+            
+            if not result: result = {}
+            
+            if not result.get('message'):
+                result['message'] = self.success_message
+            
+            data['message'] = result.get('message', 'Success') if result else 'Success'
+
+            return Response(
+                data, status=result.get('status_code', status.HTTP_200_OK)
+            )
+        except BusinessRuleException as err:
+            return Response(
+                {'status': 'error', 'detail': str(err) or RESPONSE_TENTE_NOVAMENTE}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except ValidationException as err:
+            return Response(
+                {'status': 'error', 'detail': str(err) or RESPONSE_ALGO_QUE_MANDOU_ESTA_ERRADO}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except AuthorizationException as err:
+            return Response(
+                {'status': 'error', 'detail': str(err) or RESPONSE_VOCE_NAO_PODE_FAZER_ISSO}, status=status.HTTP_403_FORBIDDEN
+            )
+        except SystemErrorException as err:
+            return Response(
+                {'status': 'error', 'detail': str(err) or RESPONSE_VOCE_NAO_PODE_FAZER_ISSO}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        except NotFoundException as err:
+            return Response(
+                {'status': 'error', 'detail': str(err) or RESPONSE_ALGUM_DADO_NAO_FOI_ENCONTRADO}, status=status.HTTP_404_NOT_FOUND
+            )
         except Exception as err:
             return Response(
                 {'status': 'error', 'detail': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
